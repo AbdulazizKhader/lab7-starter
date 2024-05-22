60bbf3121c25c0fc0e5a1ce4f8058f7d7cc805ff
AbdulazizKhader <akhader@ucsd.edu>
2024-05-21 17:05:17 -0700

    finished explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -42,10 +42,17 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	let mainEl = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	recipes.forEach((recipe) => {
+		let recipeCard = document.createElement('recipe-card');
+		recipeCard.data = recipe;
+		mainEl.appendChild(recipeCard);
+	});
+	
 }
 
 /**
